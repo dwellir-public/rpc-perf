5f00c92bcc37d459b3b8dde98818016ae5b2b29a
theduck <ankan.anurag@gmail.com>
2022-03-14 10:39:51 +0100

    download data

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
 # rpcperf
 
-RPC Node Performance Issues
+## Running test on dev nodes
 
-If you want to perform these tests on polkadot node, you can run the following command 
-to download the chain snapshot in order to sync the node faster.
+`DB_CACHE=128 CONN=20 docker-compose --profile dev up --abort-on-container-exit`
 
+
+## Running test on Polkadot prod node
+
+- This test will need to be in sync with polkadot network. To make the sync faster run the following command which will download the latest chain snapshot (~45G).
 `docker-compose run prefetch-data`
 
+- Spin up prod network and tests with 
 
-Spin up prod network and tests with 
 `DB_CACHE=128 docker-compose --profile prod up --abort-on-container-exit`
+
+
+## Building test clients
+`docker-compose --profile prod build`
+
+
+// TODO
+- make file
+- dump prometheus metrics
+- snapshot of grafana graph
+- tell how many connections
+- set db-cache
+- test configuration using env files
+- artillery
+- flamegraph node
+
+
+// less peers, more peers, less db cache, more db cache, different versions of polkadot node
+// node-version: v2.3.4
--- test-client/cpu-test.js
+++ test-client/cpu-test.js
@@ -6,7 +6,7 @@ import * as log from 'fancylog'
 
 const ADDRESS = process.env.WS_ADDRESS || 'ws://127.0.0.1:9944';
 // const ADDRESS = process.env.WS_ADDRESS || 'wss://kusama-rpc.polkadot.io';
-const CONNECTIONS = process.env.CONN || 10;
+const CONNECTIONS = process.env.CONN || 2;
 const TOTAL_BLOCKS = 256;
 const RUN_TAG = process.env.TAG || 'X';
 
@@ -15,17 +15,18 @@ async function main() {
   const provider = new WsProvider(ADDRESS);
   const api = await ApiPromise.create({ provider: provider });
   await chainSync(api);
-  
+
   let currentBlock = await api.rpc.chain.getBlock();
   const endBlock = currentBlock.block.header.number;
   log.info(`(Shell ${RUN_TAG}) Current block: ${currentBlock.block.header.number}`);
   const startBlock = endBlock - TOTAL_BLOCKS;
 
+  let startDate = new Date();
   var startTime = performance.now();
   let conn = Number.parseInt(CONNECTIONS, 10);
   let tasks = [];
   let offset = RUN_TAG * CONNECTIONS;
-  
+
   for (let i = 0; i < conn; i++) {
     tasks.push(traverseBlocks(ADDRESS, startBlock, endBlock, i + offset)
       .catch(error => log.error(error)));
@@ -33,34 +34,87 @@ async function main() {
 
   Promise.all(tasks).then(() => {
     var endTime = performance.now();
+    let endDate = new Date();
     log.info(`(Shell ${RUN_TAG}) Total running time for the test with ${endBlock - startBlock} parallel connections and ${TOTAL_BLOCKS} blocks/connection took ${(endTime - startTime) / 1000} seconds to finish`);
     log.warn(`Finished processing shell ${RUN_TAG}.`)
-    process.exit();
+
+    recordResults(startDate, endDate);
+
   }).catch(error => {
     log.error(error.message)
   });
 }
 
-async function chainSync(api){
+async function chainSync(api) {
   let syncState = await api.rpc.system.syncState();
   let highestBlock = syncState.highestBlock;
   let currentBlock = syncState.currentBlock;
   log.info(`Highest Block= ${highestBlock} and Current Block = ${currentBlock}`);
-   
+
   while (highestBlock - currentBlock > 3) {
     log.info(`Waiting for chain to complete syncing. ${highestBlock - currentBlock} to go!`);
-    await sleep(1);
+    await sleep(3);
     syncState = await api.rpc.system.syncState();
     currentBlock = syncState.currentBlock;
-    log.info(`Current Block = ${currentBlock}`);      
+    log.info(`Current Block = ${currentBlock}`);
   }
 
   log.info(`Chain is now in sync!`);
-   
+
 }
 
 async function sleep(seconds) {
   await new Promise(r => setTimeout(r, seconds * 1000));
+}
+
+async function fetchMetrics(url, filename) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        writeFile(data, filename);
+        resolve()
+      });
+  })
+
+}
+
+async function recordResults(startDate, endDate) {
+  let currentRun = startDate.getTime();
+  let dir = `./etc/tests/${currentRun}`
+  fs.mkdirSync(dir, { recursive: true })
+
+  await fetchMetrics('http://cadvisor:8080/metrics', `${dir}/metrics-cadvisor.txt`);
+  await fetchMetrics('http://node_exporter:9100/metrics', `${dir}/metrics-node-exporter.txt`);
+  await fetchMetrics('http://polkadot:9615/metrics', `${dir}/metrics-polkadot.txt`);
+  let panels = 5;
+  for (let i = 1; i <= panels; i++) {
+    await downloadImage(`http://admin:admin@grafana:3000/render/d-solo/pMEd7m0Mz/cadvisor-exporter?orgId=1&from=${startDate.getTime()}&to=${endDate.getTime}&panelId=${i}&width=1000&height=500`, `${dir}/panel-${i}.png`);
+  } 
+  log.info(`Results recorded at ${currentRun}`);
+  process.exit();
+}
+
+function writeFile(content, filepath) {
+  try {
+    fs.writeFileSync(filepath, content);
+    //file written successfully
+  } catch (err) {
+    log.error(err)
+  }
+}
+
+async function downloadImage(url, filepath) {
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  });
+  return new Promise((resolve, reject) => {
+    response.data.pipe(fs.createWriteStream(filepath))
+      .on('error', reject)
+      .once('close', () => resolve(filepath));
+  });
 }
 
 main().catch(console.error);
